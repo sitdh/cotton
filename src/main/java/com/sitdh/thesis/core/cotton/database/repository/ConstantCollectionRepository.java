4d8bde50be2056e9414d7f81346faf1e2ee3c2b2
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-05-26 15:32:15 +0700

    Add new repository for collecting constants

---
+++ src/main/java/com/sitdh/thesis/core/cotton/database/repository/ConstantCollectionRepository.java
@@ -0,0 +1,9 @@
+package com.sitdh.thesis.core.cotton.database.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sitdh.thesis.core.cotton.database.entity.ConstantCollection;
+
+public interface ConstantCollectionRepository extends JpaRepository<ConstantCollection, String> {
+
+}
