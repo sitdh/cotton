59700ecf0e21a478eae5ede90078b9c4bb16fe48
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-05-27 21:59:46 +0700

    Add new entity for keeping graph

--- src/main/java/com/sitdh/thesis/core/cotton/database/entity/Project.java
+++ src/main/java/com/sitdh/thesis/core/cotton/database/entity/Project.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -22,17 +23,28 @@ public class Project {
 	private String projectId;
 	
 	@Getter @Setter
-	@Column(name="owner", length=50)
+	@Column(name="owner", length=50, nullable=true)
 	private String owner;
 	
 	@Getter @Setter
 	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="container")
 	private List<FileName> files;
 	
+	@Getter @Setter @Lob
+	@Column(name="graph_graph", nullable=true)
+	private String graphClass;
+	
+	@Getter @Setter @Lob
+	@Column(name="graph_method", nullable=true)
+	private String graphMethod;
+	
+	@Getter @Setter
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="projectId")
+	private List<ControlFlowGraph> graphs;
+	
 	public Project() { }
 	
-	public Project(String projectId, String owner) {
+	public Project(String projectId) {
 		this.setProjectId(projectId);
-		this.setOwner(owner);
 	}
 }
